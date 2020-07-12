109b08a59c6c0cb77e7e873a6a86a2c9d26f606a
Akimmax <Akimmax>
2020-07-12 10:12:53 +0300

    Add ability to run Agents

---
+++ Agent/Agent/AppOptions.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace Agent
+{
+
+    class AppOptions
+    {
+
+        public void initalOptions(string[] args)
+        {
+            Console.WriteLine("initalOptions " + args.ToString());
+            this.agentIp = args[0];
+            this.agentPort = int.Parse(args[1]);
+            this.agentId = int.Parse(args[2]);
+
+            this.clientIp1 = args[3];
+            this.clientPort1 = int.Parse(args[4]);
+            this.clientId1 = int.Parse(args[5]);
+
+            this.clientIp2 = args[6];
+            this.clientPort2 = int.Parse(args[7]);
+            this.clientId2 = int.Parse(args[8]);
+
+            this.currentPosition = getPoints(args[9]);
+            this.targetPosition = getPoints(args[10]);
+
+            this.currentPosition3D = get3DPoint(args[9]);
+            this.targetPosition3D = get3DPoint(args[10]);
+
+        }
+        //==>TODO Rework using reflection
+        public void printOptions()
+        {
+            Console.WriteLine("printOptions ");
+            Console.WriteLine(this.agentIp);
+            Console.WriteLine(this.agentPort);
+            Console.WriteLine(this.agentId);
+
+            Console.WriteLine(this.clientIp1);
+            Console.WriteLine(this.clientPort1);
+            Console.WriteLine(this.clientId1);
+
+            Console.WriteLine(this.clientIp2);
+            Console.WriteLine(this.clientPort2);
+            Console.WriteLine(this.clientId2);
+            Console.WriteLine(this.currentPosition);
+            Console.WriteLine(this.targetPosition);
+            Console.WriteLine(this.currentPosition3D);
+            Console.WriteLine(this.targetPosition3D);
+
+        }
+
+        static public Point getPoints(string args)
+        {
+            var points = args.Split(',').ToList<string>();
+            var ints = points.Select(str => {
+                bool success = int.TryParse(str, out int value);
+                return new { value, success };
+            }).Where(pair => pair.success).Select(pair => pair.value).ToArray();
+
+            return new Point(ints[0], ints[1]);
+        }
+
+        static public Point3D get3DPoint(string args)
+        {
+            var parameters = args.Split(',').ToList<string>();
+            var coordinates = parameters.Select(str => {
+                return Convert.ToDouble(int.Parse(str));
+            }).ToArray();
+
+            return new Point3D(coordinates[0], coordinates[1], coordinates[2]);
+        }
+
+
+        public string agentIp;
+        public int agentPort;
+        public int agentId;
+        public string clientIp1;
+        public int clientPort1;
+        public int clientId1;
+        public string clientIp2;
+        public int clientPort2;
+        public int clientId2;
+        public Point currentPosition;
+        public Point targetPosition;
+        public Point3D currentPosition3D;
+        public Point3D targetPosition3D;
+    }
+}
--- Agent/Agent/Program.cs
+++ Agent/Agent/Program.cs
@@ -14,7 +14,18 @@ namespace Agent
     {
         static void Main(string[] args)
         {
-            
+            AppOptions opt = new AppOptions();
+            opt.initalOptions(args);
+
+            var tcpAgent = new Agent(System.Net.IPAddress.Parse(opt.agentIp), opt.agentPort, opt.agentId);
+            tcpAgent.SetPosition2(new Point3D(opt.currentPosition3D.X, opt.currentPosition3D.Y, opt.currentPosition3D.Z));
+            tcpAgent.StartService();
+            Thread.Sleep(1000);
+
+            var t3 = Task.Run(() => tcpAgent.MoveToPoint2(new Point3D(opt.targetPosition3D.X, opt.targetPosition3D.Y, opt.targetPosition3D.Z)));
+            var t7 = Task.Run(() => tcpAgent.RunClient(IPAddress.Parse(opt.clientIp1), opt.clientPort1, opt.clientId1));
+            var t8 = Task.Run(() => tcpAgent.RunClient(IPAddress.Parse(opt.clientIp2), opt.clientPort2, opt.clientId2));
+
             Console.ReadKey();
         }
     }
