32bd1139e7f97c34656282bf1703d79827cabcb7
Akimmax <Akimmax>
2020-07-12 09:50:35 +0300

    Initial Commit

---
+++ Agent/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
