441b688f961d2f82e74f0f4e7a3b9beb7aceba74
Akimmax <Akimmax>
2020-08-01 17:54:54 +0300

    Do some refactoring

--- AgentVisualisator/AgentVisualisator/Form1.cs
+++ AgentVisualisator/AgentVisualisator/Form1.cs
@@ -1,5 +1,4 @@
 using System;
-//using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows;
@@ -16,67 +15,25 @@ namespace AgentVisualisator
         {
             InitializeComponent();
             myDelegate = new SafeCallDelegate(runWriteTextSafe2);
-            drowOnForm = new DrowCallDelegate(DrawPointOnForm);
-
-            //=============================Graphics
-            drowOnChart = new DrowOnChartDelegate(DrawPointOnChart);
             drowOnChart3D = new DrowOnChartDelegate3D(DrawPointOnChart2);
             chartHelper = new Chart3DGraphicsVisualisator();
             chartHelper.prepare3dChart(this.chart1, this.chartArea1, maxSeriesNumber);
 
         }
-        public delegate void SafeCallDelegate(string text);//TODO 
-        public SafeCallDelegate myDelegate;//TODO 
-        public delegate void DrowCallDelegate(Point p);
-        public DrowCallDelegate drowOnForm;
-
-
-        //System.Drawing.Bitmap image1 = new System.Drawing.Bitmap(@"D:\University\4.png", true);
-        //=============================Graphics
-        public delegate void DrowOnChartDelegate(Point p, int seriesId);
+        public delegate void SafeCallDelegate(string text);//TODO  Rename
+        public SafeCallDelegate myDelegate;//TODO  Rename 
         public delegate void DrowOnChartDelegate3D(System.Windows.Media.Media3D.Point3D p, int seriesId);
-        public DrowOnChartDelegate3D drowOnChart3D;
-        public DrowOnChartDelegate drowOnChart;
+        public DrowOnChartDelegate3D drowOnChart3D;//TODO  Rename 
+
         Chart3DGraphicsVisualisator chartHelper;
         int maxSeriesNumber = 10;
 
-
-        public void DrowOnForm(Point p, string name, int step)
+        private void runWriteTextSafe2(string text)//TODO  Rename 
         {
-            System.Drawing.Brush aBrush = (System.Drawing.Brush)System.Drawing.Brushes.Red;
-            System.Drawing.Graphics g = CreateGraphics();
-            g.FillRectangle(aBrush, (int)p.X, (int)p.Y, 3, 3);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void DrawPointOnForm(Point p)//TODO Remove we have 3d 
-        {
-            System.Drawing.Color newColor = System.Drawing.Color.FromName("SlateBlue");//Color.FromArgb(pixelColor.R, 0, 0);
-            //image1.SetPixel((int)p.X, (int)p.Y, newColor);
-            //pictureBox1.Image = image1;
-        }
-
-        private void runWriteTextSafe2(string text)
-        {
-            //textBox1.Text = text;//==>TODO remove
-            //textBox1.Text += (text +"\r\n");
             listBox1.Items.Add(text);
         }
 
-        private void DrawPointOnChart(Point p, int seriesId)
-        {
-
-
-            chartHelper.AddXY3d(this.chart1.Series[seriesId], p.X, p.Y, 0);
-            string mess = "Drown on chart " + seriesId + " " + p.X + " " + p.Y;
-            runWriteTextSafe2(mess);
-        }
-
-        private void DrawPointOnChart2(System.Windows.Media.Media3D.Point3D p, int seriesId)
+        private void DrawPointOnChart2(System.Windows.Media.Media3D.Point3D p, int seriesId)//TODO  Rename 
         {
             //!!! IMPORTANT On chart Y is Z, Z is Y. Because we should swap params
             chartHelper.AddXY3d(this.chart1.Series[seriesId], p.X, p.Z, p.Y);
@@ -86,11 +43,6 @@ namespace AgentVisualisator
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //pictureBox1.Image = image1;
-            System.Drawing.Color newColor = System.Drawing.Color.FromName("SlateBlue");
-            //image1.SetPixel(100, 150, newColor);
-
-
             var tcpAgent = new TCPClient(0);
             tcpAgent.form = this;
             string ip = "127.0.0.1";
@@ -100,37 +52,13 @@ namespace AgentVisualisator
             int port2 = 38246;
             int port3 = 38244;
 
-
             var t2 = Task.Run(() => tcpAgent.RunClient(IPAddress.Parse(ip), port1, 1));
             var t1 = Task.Run(() => tcpAgent.RunClient(IPAddress.Parse(ip2), port2, 2));
-            var t3 = Task.Run(() => tcpAgent.RunClient(IPAddress.Parse(ip3), port3, 3));
-            //var t4 = Task.Run(() => tcpAgent.RunClient(IPAddress.Parse("127.0.0.1"), 38243, 4));
-
-
-
-            //try
-            //{
-            //   //t1.Wait();
-            //}
-            //catch (AggregateException ae)
-            //{
-            //    foreach (var ec in ae.InnerExceptions)
-            //    {
-            //        // Handle the custom exception.
-            //        if (ec is Exception)
-            //        {
-            //            Console.WriteLine(ec.Message);
-            //        }
-            //        // Rethrow any other exception.
-            //        else
-            //        {
-            //            throw;
-            //        }
-            //    }
-            //}
+            var t3 = Task.Run(() => tcpAgent.RunClient(IPAddress.Parse(ip3), port3, 3));            
         }
 
-        private void chart1_PostPaint(object sender, System.Windows.Forms.DataVisualization.Charting.ChartPaintEventArgs e)
+        //used for 3D drawing on chart
+        private void chart1_PostPaint(object sender, ChartPaintEventArgs e)
         {
             Chart chart = sender as Chart;
             if (chart.Series.Count < 1) return;
@@ -139,9 +67,5 @@ namespace AgentVisualisator
             chartHelper.HandlePaintEventForChart(chart, e);
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
--- AgentVisualisator/AgentVisualisator/TCPClient.cs
+++ AgentVisualisator/AgentVisualisator/TCPClient.cs
@@ -28,29 +28,27 @@ namespace AgentVisualisator
             var query =
               (from value in client.Query()
                select value);
+
             using (query.Subscribe(
               value => {
                   var messsege = JsonSerializer.Deserialize<TCPMessage>(value); ////==> TODO extract to log
                   var str = $"Agent #{ _agentId}, Client #{name} received message from {messsege.SenderId} :X = {messsege.CurrentAgentPosition.X} ; Y  = {messsege.CurrentAgentPosition.Y} ;  z  = {messsege.CurrentAgentPosition.Z} === {DateTime.Now.ToString("hh:mm:ss.fff")} ";
                   PrintText(str);
 
-                  // DisplayOnForm(messsege.currentPosition3D);
                   DisplayOnChart3D(messsege.CurrentAgentPosition, messsege.SenderId);
               },
               ex =>
               {
                   PrintText($"Error In agent #{_agentId} in client #{name}: {ex.Message}");
-
               },
               () => {
                   Console.WriteLine("In agent #{0} client #{1} completed", _agentId, name);
               }
               ))
-            {
-                // PrintText($"Agent #{_agentId} client #{name} started.  Waiting for basic service notifications...");
-                //Console.ReadKey(intercept: true); ////==> TODO remove 
+            {            
+                
                 var x = Console.Read();
-                Thread.Sleep(50000);////==> TODO remove 
+                Thread.Sleep(50000);////==> TODO remove //Console.ReadKey(intercept: true); ////==> TODO remove 
             }
         }
         public void PrintText(string myString)
@@ -58,24 +56,11 @@ namespace AgentVisualisator
             form.Invoke(form.myDelegate, new object[] { myString });
         }
 
-        public void DisplayOnForm(Point position)
-        {
-            form.Invoke(form.drowOnForm, new object[] { position });
-        }
-
         public void DisplayOnChart3D(Point3D position, int agentId)
         {
             form.Invoke(form.drowOnChart3D, new object[] { position, agentId });
             string mess = "Try drow on chart " + agentId + " " + position.X + " " + position.Y;
             PrintText(mess);
-        }
-
-        public void DisplayOnChart(Point3D position, int agentId)
-        {
-            form.Invoke(form.drowOnChart, new object[] { position, agentId });
-            string mess = "Try drow on chart " + agentId + " " + position.X + " " + position.Y + " " + position.Z;
-            PrintText(mess);
-
         }
 
     }
